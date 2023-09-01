a8578f0818cada4995a01ca3beb4078641492d42
Mehdi <mehdi1.zenati@epitech.eu>
2023-09-01 20:31:07 +0200

    ça marchera bien au bout d'un moment

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -68,7 +68,8 @@ module.exports = {
           // w3c:false tells Chromedriver to run using the legacy JSONWire protocol (not required in Chrome 78)
           w3c: true,
           args: [
-            //'--no-sandbox',
+            '--no-sandbox',
+            '--disable-dev-shm-usage'
             //'--ignore-certificate-errors',
             //'--allow-insecure-localhost',
             //'--headless'
