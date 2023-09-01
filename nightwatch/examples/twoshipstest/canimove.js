3b5151e961de7c5d405a726ccbf488f01b17b8ed
Mehdi <mehdi1.zenati@epitech.eu>
2023-09-01 16:50:20 +0200

    test

---
+++ nightwatch/examples/twoshipstest/canimove.js
@@ -0,0 +1,39 @@
+describe('test souris', function() {
+    // before(browser => browser.navigateTo('http://127.0.0.1:5500/TwoShips/index.html'));
+    it('Can I start game', function(client) {
+        client.url("http://127.0.0.1:5500/TwoShips/index.html")
+      client
+        .click('.r')
+        .waitForElementVisible('#canvas', 5000)
+
+      console.log("game Started");
+  
+    });
+
+    it("Can shoot", function(client)  {
+        client
+        .pause(1000) 
+        .clickAndHold(".r")
+      .pause(5000)
+      .click(".h")
+      //Désolé je n'ai pas réussi à trigger une assertion de façon propre
+      client.waitForElementVisible('#canvas', 100)      
+      });
+
+      it("Can kill", function (client){
+        client.getText(".s",  function(result) {
+          console.log('score =>', result.value)
+          if(result.value > 200){
+            //Désolé je n'ai pas réussi à trigger une assertion de façon propre
+            client.assert.ok(result.value > 200)
+          }
+      })
+    })
+  });
+
+  
+  
+  
+  
+  
+  
---
+++ nightwatch/examples/twoshipstest/canishoot.js
@@ -0,0 +1,17 @@
+describe('test clavier', function() {
+    it('Can I start game', function(client) {
+        client.url("http://127.0.0.1:5500/TwoShips/index.html")
+     client
+        .click('.r')
+        .waitForElementVisible('#canvas', 5000)
+      console.log("game Started");
+    });
+    it('my body is ready !!', function(client) {  
+    for(i = 0; i<100; i++){
+        client.sendKeys("body", "sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss")
+    };
+    client
+    .waitForElementVisible('.e', 5000)
+    .assert.screenshotIdenticalToBaseline('body',  /* Optional */ 'fallingtest', {threshold: 0.1}, 'Photos comparées.');
+});
+})
